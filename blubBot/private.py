27ac4c1c83a8defd6ea16c5859ccc8fa2373365f
Austin Colt <austin@jcolt.net>
2024-06-13 21:30:46 -0600

    Get ready for docker deployment

--- blubBot/private.py
+++ blubBot/private.py
@@ -1,7 +1,7 @@
 class Private:
     # Fix class to do what I need it to do. Needs to be a helper class
 
-    with open("secret_sauce.csv") as secretFile:
+    with open("blubBot/secret_sauce.csv") as secretFile:
         next(secretFile)
         for line in secretFile:
             line.strip()
